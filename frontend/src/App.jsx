0892264f576d624ad3db0cc787d02e0d57f48ba8
Fathur <114320333+fatkhurrhn@users.noreply.github.com>
2024-11-28 00:13:50 +0700

    add job & culture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage.jsx";
+import Jobfit from "./pages/job-fit.jsx";
+import Culturefit from "./pages/culture-fit.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/job-fit" element={<Jobfit />} />
+      <Route path="/culture-fit" element={<Culturefit />} />
     </Routes>
   );
 }
