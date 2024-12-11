9eaac072fd7d9fcef753c5b6228bc89ad83ab051
Haruki <jimmymaulana01@gmail.com>
2024-12-11 22:18:31 +0700

    config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,23 @@ import CoreValue from "./pages/coreValuePage.jsx";
 import LoginPage from "./pages/Loginpage.jsx"
 import Jobfit from "./pages/job-fit.jsx";
 import Culturefit from "./pages/culture-fit.jsx";
+import DetailProfile from "./pages/detailProfile.jsx";
+import UserProfile from "./pages/userProfile.jsx";
+import FindPage from "./pages/findpage.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/DetailProfil" element={<DetailProfile/>} />
+      <Route path="/UserProfile" element={<UserProfile/>} />
       <Route path="/Employee" element={<Employee />} />
       <Route path="/Corevalue" element={<CoreValue />} />
       <Route path="/Loginpage" element={<LoginPage />} />
       <Route path="/job-fit" element={<Jobfit />} />
       <Route path="/culture-fit" element={<Culturefit />} />
+      <Route path="/MatchingByPerson" element={<FindPage />} />
     </Routes>
   );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://103.190.214.174:5017',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/chat': {
+        target: 'http://103.190.214.174:5011',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/chat/, ''),
+      },
+      '/find': {
+        target: 'http://103.190.214.174:5002',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/find/, ''),
+      },
+    },
+  },
 })
